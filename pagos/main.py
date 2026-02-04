3fd0706594e0fb060b536f345965bcae5c0c7f4f
Sebastián Sinchi Naula <151038925+elsebasdev1@users.noreply.github.com>
2026-02-03 22:46:31 -0500

    Update main.py

--- pagos/main.py
+++ pagos/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import asyncio
+import redis.asyncio as redis # Necesario para leer la bandera de caos
 
 app = FastAPI()
+
+# Cliente Redis para leer flags de caos
+redis_client = redis.from_url("redis://localhost:6379", decode_responses=True)
 
 app.add_middleware(
     CORSMiddleware,
@@ -15,7 +19,19 @@ app.add_middleware(
 async def procesar_pago():
     try:
         print("--> Recibiendo petición de pago...")
-        await asyncio.sleep(1) # El servicio de pagos demora 20 segundos por una sobrecarga.
+        
+        # --- CHAOS MONKEY: LA PASARELA LENTA ---
+        # Verificamos si el modo caos está activado
+        modo_lento = await redis_client.get("chaos:pagos:latencia")
+        
+        if modo_lento == "true":
+            print("CHAOS ACTIVADO: Simulando latencia de 20 segundos...")
+            await asyncio.sleep(20) # Esto forzará un Timeout en el servicio de Reservas
+        else:
+            # Latencia normal simulada (1 segundo)
+            await asyncio.sleep(1) 
+        # ---------------------------------------
+
         # Enviamos un JSON claro y directo
         return {"status": "success", "mensaje": "Aprobado"}
     except Exception as e:
